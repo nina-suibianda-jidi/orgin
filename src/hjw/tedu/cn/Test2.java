e4c5a2642c38a96782fdc4ada3e0b5301ec62ab4
hjw <1016888576@qq.com>
2021-07-08 10:09:09 +0800

    none

--- src/hjw/tedu/cn/Test2.java
+++ src/hjw/tedu/cn/Test2.java
@@ -8,6 +8,6 @@ public class Test2 {
         System.out.println("hello git4");
         System.out.println("mast hello");
         System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("???");
     }
 }
